70402c68ca66bb72927a3c8fec45c883f5e132d0
Ariyanti-idris27 <ariyantiidris72@gmail.com>
2025-01-01 22:58:33 +0900

    Update main_pbo.py

--- main_pbo.py
+++ main_pbo.py
@@ -10,3 +10,19 @@ print ("\nTest Pesan : ", p.mes(), "\n")
 
 # silahkan uji fungsi dan class anda dibawah
 # pastikan class dan fungsi sudah anda buat di modul_satu_pbo.py
+
+
+# Import kelas dari file kelas_mahasiswa.py
+from kelas_mahasiswa import Mahasiswa, MahasiswaTeknik, MahasiswaEkonomi
+
+# Main Program
+if __name__ == "__main__":
+    # Membuat objek dari masing-masing kelas
+    mhs1 = Mahasiswa("Ariyanti", "23072")
+    mhs2 = MahasiswaTeknik("Budi", "987654321", "Teknik Informatika")
+    mhs3 = MahasiswaEkonomi("Citra", "567890123", "Manajemen Keuangan")
+
+    # Menampilkan data mahasiswa
+    print(mhs1.tampilkan_data())
+    print(mhs2.tampilkan_data())
+    print(mhs3.tampilkan_data())
