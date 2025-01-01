6c4fba020a070e5dd493e0add2bc3c3af3f86d42
Ariyanti-idris27 <ariyantiidris72@gmail.com>
2025-01-01 22:53:21 +0900

    Update modul_satu_pbo.py

--- modul_satu_pbo.py
+++ modul_satu_pbo.py
@@ -13,3 +13,33 @@ class test:
 
 # silahkan lanjutkan dengan fungsi dan calss anda dibawah
 # pastikan untuk menguji class dan fungsi yang sudah di buat disini
+
+# Definisi kelas Mahasiswa dan kelas turunannya
+
+class Mahasiswa:
+    def __init__(self, nama, nim):
+        self.nama = nama
+        self.nim = nim
+
+    def tampilkan_data(self):
+        return f"Nama: {self.nama}, NIM: {self.nim}"
+
+# Class turunan dari Mahasiswa (Inheritance)
+class MahasiswaTeknik(Mahasiswa):
+    def __init__(self, nama, nim, jurusan):
+        super().__init__(nama, nim)
+        self.jurusan = jurusan
+
+    def tampilkan_data(self):
+        return f"Nama: {self.nama}, NIM: {self.nim}, Jurusan: {self.jurusan}"
+
+# Class turunan lain untuk Mahasiswa Ekonomi (Polymorphism)
+class MahasiswaEkonomi(Mahasiswa):
+    def __init__(self, nama, nim, konsentrasi):
+        super().__init__(nama, nim)
+        self.konsentrasi = konsentrasi
+
+    def tampilkan_data(self):
+        return f"Nama: {self.nama}, NIM: {self.nim}, Konsentrasi: {self.konsentrasi}"
+
+
